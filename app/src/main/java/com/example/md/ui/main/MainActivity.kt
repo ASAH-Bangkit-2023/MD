e93a6ff2f853a069f370af82c882cf110218947c
waniAEbro <wani43bro@students.unnes.ac.id>
2023-05-13 19:36:03 +0700

    make bottom navigation make navigation controller

--- app/src/main/java/com/example/md/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/md/ui/main/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.md.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.md.R
+import com.example.md.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.bottomNavigation
+
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_maps,
+                R.id.navigation_about,
+                R.id.navigation_profile
+            )
+        )
+
+        navView.setupWithNavController(navController)
     }
 }
