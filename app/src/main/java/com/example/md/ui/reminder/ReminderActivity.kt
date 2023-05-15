151ba39c6e61d63cc6ac84d634e525879058202d
waniAEbro <wani43bro@students.unnes.ac.id>
2023-05-15 11:19:18 +0700

    make time picker UI for reminder

--- app/src/main/java/com/example/md/ui/reminder/ReminderActivity.kt
+++ app/src/main/java/com/example/md/ui/reminder/ReminderActivity.kt
@@ -3,13 +3,35 @@ package com.example.md.ui.reminder
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.md.databinding.ActivityReminderBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class ReminderActivity : AppCompatActivity() {
+class ReminderActivity : AppCompatActivity(), TimePickerFragment.DialogTimeListener {
     private lateinit var binding: ActivityReminderBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityReminderBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.clockButton.setOnClickListener {
+            val timePickerFragmentOne = TimePickerFragment()
+            timePickerFragmentOne.show(supportFragmentManager, TIME_PICKER_TAG)
+        }
+    }
+
+    override fun onDialogTimeSet(tag: String?, hourOfDay: Int, minute: Int) {
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+        calendar.set(Calendar.MINUTE, minute)
+        val dateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+        if (tag == TIME_PICKER_TAG) {
+            binding.jam.text = dateFormat.format(calendar.time)
+        }
+    }
+
+    companion object {
+        private const val TIME_PICKER_TAG = "TimePicker"
     }
 }
