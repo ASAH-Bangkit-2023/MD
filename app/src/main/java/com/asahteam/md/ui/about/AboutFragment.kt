fb9369a192741acb2551315e7892d0356e6f8f8c
waniAEbro <wani43bro@students.unnes.ac.id>
2023-06-15 17:13:05 +0700

    make linkedin intent at about page

--- app/src/main/java/com/asahteam/md/ui/about/AboutFragment.kt
+++ app/src/main/java/com/asahteam/md/ui/about/AboutFragment.kt
@@ -1,5 +1,7 @@
 package com.asahteam.md.ui.about
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +19,54 @@ class AboutFragment : Fragment() {
     ): View? {
         _binding = FragmentAboutBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.let { binding ->
+            binding.ourProfileImage1.setOnClickListener {
+                openLinkedIn("https://www.linkedin.com/in/fikriiardiansyahh/")
+            }
+            binding.ourProfileImage2.setOnClickListener {
+                openLinkedIn("https://www.linkedin.com/in/zikrikn/")
+            }
+            binding.ourProfileImage3.setOnClickListener {
+                openLinkedIn("https://www.linkedin.com/in/ridwan-ridlo-nugroho-617a10223/")
+            }
+            binding.ourProfileImage4.setOnClickListener {
+                openLinkedIn("https://www.linkedin.com/in/irzanluthfi/")
+            }
+            binding.ourProfileImage5.setOnClickListener {
+                openLinkedIn("https://www.linkedin.com/in/edwin-mahendra/")
+            }
+            binding.ourProfileImage6.setOnClickListener {
+                openLinkedIn("https://www.linkedin.com/in/adiatmaja/")
+            }
+            binding.ourProfileName1.setOnClickListener {
+                openLinkedIn("https://www.linkedin.com/in/fikriiardiansyahh/")
+            }
+            binding.ourProfileName2.setOnClickListener {
+                openLinkedIn("https://www.linkedin.com/in/zikrikn/")
+            }
+            binding.ourProfileName3.setOnClickListener {
+                openLinkedIn("https://www.linkedin.com/in/ridwan-ridlo-nugroho-617a10223/")
+            }
+            binding.ourProfileName4.setOnClickListener {
+                openLinkedIn("https://www.linkedin.com/in/irzanluthfi/")
+            }
+            binding.ourProfileName5.setOnClickListener {
+                openLinkedIn("https://www.linkedin.com/in/edwin-mahendra/")
+            }
+            binding.ourProfileName6.setOnClickListener {
+                openLinkedIn("https://www.linkedin.com/in/adiatmaja/")
+            }
+        }
+    }
+
+    private fun openLinkedIn(link: String) {
+        val intent =
+            Intent(Intent.ACTION_VIEW, Uri.parse(link))
+        startActivity(intent)
     }
 
     override fun onDestroy() {
